bb52a1df64b35682436bbd52faeb15950f75f135
web-davy <the-davy-devv@gmail.com>
2026-07-26 21:27:56 +0300

    New missions

--- js/data/missions/Bert/community-outreach.js
+++ js/data/missions/Bert/community-outreach.js
@@ -5,7 +5,7 @@ const MISSIONS_BERT_COMMUNITY_OUTREACH = [
     showMoreButton: true,
     title: "Community Outreach",
     location: "Police Station – Talk to Bert",
-    description: "",
+    description: "A big part of the job is putting on a friendly face for the community. Being there for your neighbors. Go out there, knock on some doors, and shake some hands. Get to know the people you're fighting for.",
     howToComplete: "Patrol Suncrest Hills and Oakwood Estate for 5 minutes",
     difficulty: "EASY",
     missionType: "Bert",
