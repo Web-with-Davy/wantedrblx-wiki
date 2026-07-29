cd47f00d10f1c18d6e8777e3105ea1f753001153
web-davy <the-davy-devv@gmail.com>
2026-07-29 19:51:32 +0300

    New missions

--- js/data/missions/Bert/loss-prevention.js
+++ js/data/missions/Bert/loss-prevention.js
@@ -1,7 +1,7 @@
 const MISSIONS_BERT_LOSS_PREVENTION = [
 
   {
-    id: "loss-prevention-1",
+    id: "loss-prevention",
     showMoreButton: true,
     title: "Loss Prevention - Part 1",
     location: "Police Station – Talk to Bert",
@@ -12,4 +12,15 @@ const MISSIONS_BERT_LOSS_PREVENTION = [
     rewards: ["500 Police XP", "$5,000"]
   },
 
+  {
+    id: "loss-prevention",
+    showMoreButton: true,
+    title: "Loss Prevention - Part 2",
+    location: "Police Station – Talk to Bert",
+    description: "We got a tip that a crew is going to hit the Pear Store. Go stake it out and roll in when they least expect it.",
+    howToComplete: "PAtrol Pear Store for 15 minutes and confiscate 10 Gold PearTech items",
+    difficulty: "MEDIUM",
+    missionType: "Bert",
+    rewards: ["700 Police XP", "$10,000"]
+  }
 ];
---
+++ js/data/missions/Bert/tactical-unit.js
@@ -0,0 +1,15 @@
+const MISSIONS_BERT_TACTICAL_UNIT = [
+
+  {
+    id: "tactical-unit",
+    showMoreButton: true,
+    title: "Tactical Unit",
+    location: "Police Station – Talk to Bert",
+    description: "It's time to familiarize yourself with special weapons and tactics, S.W.A.T.\n\nHostage rescue. High-risk warrants and standoffs. Counter-terrorism. These all require specialized tools, and a unique approach.",
+    howToComplete: "Take down 10 criminals using P90",
+    difficulty: "EASY",
+    missionType: "Bert",
+    rewards: ["900 Police XP", "$50,000"]
+  },
+
+];
