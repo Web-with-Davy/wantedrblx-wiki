9d082520e257d77b756f75dbf3e10e731bf9c1bd
web-davy <the-davy-devv@gmail.com>
2026-07-30 21:34:48 +0300

    new mission

--- js/data/missions/Bert/public-relations.js
+++ js/data/missions/Bert/public-relations.js
@@ -35,4 +35,16 @@ const MISSIONS_BERT_PUBLIC_RELATIONS = [
     rewards: ["600 Police XP", "$15,000"],
     tipsAndInfo: []
   },
+
+  {
+    id: "public-relations-4",
+    title: "Public Relations - Part 4",
+    location: "Police Station – Talk to Bert",
+    description: "It's time to trade in that boring cruiser of yours for something more exciting. Our image, as a department, is important. What else are we going to do with all that tax money?",
+    howToComplete: "Patrol for 30 minutes in a Zorvello",
+    difficulty: "MEDIUM",
+    missionType: "Bert",
+    rewards: ["900 Police XP", "$30,000"],
+    tipsAndInfo: []
+  }
 ];
