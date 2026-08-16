bae86882dc4a69aacea8a3279049999fe3da6124
web-davy <the-davy-devv@gmail.com>
2026-08-16 21:28:20 +0300

    fix

--- js/data-bundle.js
+++ js/data-bundle.js
@@ -20,14 +20,14 @@ window.ATMS_DATA = [
 /* --- js/data/atms/vaults.js --- */
 window.VAULTS_DATA = [
 
-  { name: "Regular Vault", rarity: "COMMON", rarityPercent: "1 in 1.86 [calculated]", priceMin: 5000, priceMax: 7500 },
-  { name: "Silver Vault", rarity: "COMMON", rarityPercent: "1 in 3.76 [calculated]", priceMin: 7500, priceMax: 9000 },
-  { name: "Gold Vault", rarity: "UNCOMMON", rarityPercent: "1 in 6.00 [calculated]", priceMin: 10000, priceMax: 20000 },
-  { name: "Sapphire Vault", rarity: "EPIC", rarityPercent: "1 in 47.46 [calculated]", priceMin: 30000, priceMax: 40000 },
-  { name: "Ruby Vault", rarity: "MYTHIC", rarityPercent: "1 in 230.50 [calculated]", priceMin: 60000, priceMax: 70000 },
-  { name: "Emerald Vault", rarity: "LEGENDARY", rarityPercent: "1 in 403.38 [calculated]", priceMin: 100000, priceMax: 150000 },
-  { name: "Amethyst Vault", rarity: "LEGENDARY", rarityPercent: "1 in 1613.50 [calculated]", priceMin: 250000, priceMax: 275000 },
-  { name: "Diamond Vault", rarity: "UNOBTAINABLE", rarityPercent: "Admin Abuse Only", priceMin: 300000, priceMax: 325000 },
+  { name: "Regular Vault",  rarity: "COMMON",      rarityPercent: "53.76%",          priceMin: 5000,   priceMax: 7500   },
+  { name: "Silver Vault",   rarity: "COMMON",      rarityPercent: "26.60%",          priceMin: 7500,   priceMax: 9000   },
+  { name: "Gold Vault",     rarity: "UNCOMMON",    rarityPercent: "16.67%",          priceMin: 10000,  priceMax: 20000  },
+  { name: "Sapphire Vault", rarity: "EPIC",        rarityPercent: "2.11%",           priceMin: 30000,  priceMax: 40000  },
+  { name: "Ruby Vault",     rarity: "MYTHIC",      rarityPercent: "0.43%",           priceMin: 60000,  priceMax: 70000  },
+  { name: "Emerald Vault",  rarity: "LEGENDARY",   rarityPercent: "0.25%",           priceMin: 100000, priceMax: 150000 },
+  { name: "Amethyst Vault", rarity: "LEGENDARY",   rarityPercent: "0.062%",          priceMin: 250000, priceMax: 275000 },
+  { name: "Diamond Vault",  rarity: "UNOBTAINABLE",rarityPercent: "Admin Abuse Only",priceMin: 300000, priceMax: 325000 },
 
 ];
 
